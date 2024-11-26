60b8fe029814bc2092a9bcc8092fa34cb870a36a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-11-26 18:27:55 +0200

    Bump some versions. Fixup bumper.

--- cookiecutter-json-update.sh
+++ cookiecutter-json-update.sh
@@ -12,10 +12,19 @@ update_field() {
 }
 
 uv pip compile --allow-unsafe --quiet cookiecutter-packages.in --output-file=cookiecutter-packages.txt
-update_field uv_version "$(grep 'uv==' cookiecutter-packages.txt | cut -d= -f3)"
-update_field django_version "$(grep 'django==' cookiecutter-packages.txt | cut -d= -f3 | cut -d. -f1-2)"
-update_field pip_version "$(grep 'pip==' cookiecutter-packages.txt | cut -d= -f3)"
-update_field pip_tools_version "$(grep 'pip-tools==' cookiecutter-packages.txt | cut -d= -f3)"
-update_field setuptools_version "$(grep 'setuptools==' cookiecutter-packages.txt | cut -d= -f3)"
+update_field django_version "$(grep -oP '(?<=django==).*' cookiecutter-packages.txt | cut -d. -f1-2)"
+update_field pip_tools_version "$(grep -oP '(?<=pip-tools==).*' cookiecutter-packages.txt)"
+update_field pip_version "$(grep -oP '(?<=pip==).*' cookiecutter-packages.txt)"
+update_field dockerfile_version "$(grep -oP '(?<="_dockerfile_version": ")(.*)(?=")' cookiecutter.json)"
+update_field memcached_version "$(grep -oP '(?<="_memcached_version": ")(.*)(?=")' cookiecutter.json)"
+update_field postgresql_version "$(grep -oP '(?<="_postgresql_version": ")(.*)(?=")' cookiecutter.json)"
+update_field python_version "$(grep -oP '(?<="_python_version": ")(.*)(?=")' cookiecutter.json)"
+update_field redis_version "$(grep -oP '(?<="_redis_version": ")(.*)(?=")' cookiecutter.json)"
+update_field setuptools_version "$(grep -oP '(?<=setuptools==).*' cookiecutter-packages.txt)"
+update_field ubuntu_release "$(grep -oP '(?<="_ubuntu_release": ")(.*)(?=")' cookiecutter.json)"
+update_field ubuntu_version "$(grep -oP '(?<="_ubuntu_version": ")(.*)(?=")' cookiecutter.json)"
+update_field uv_version "$(grep -oP '(?<=uv==).*' cookiecutter-packages.txt)"
+
+
 rm cookiecutter-packages.txt
 
