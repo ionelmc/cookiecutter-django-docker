b8ca968ab1675b395f20451f98a23f19dc3be413
Ionel Cristian Mărieș <contact@ionelmc.ro>
2025-08-25 15:14:49 +0300

    Proper bump deps.

--- cookiecutter-json-update.sh
+++ cookiecutter-json-update.sh
@@ -3,27 +3,37 @@ set -euxo pipefail
 shopt -s extglob
 
 update_field() {
-  set +x
-  field="$1"
-  value="$2"
-  cookiecutter_json=$(cat cookiecutter.json | jq ._$field=\"$value\" | jq ".__prompts__.$field=\"$field (latest=$value)\"" )
-  echo "$cookiecutter_json" > cookiecutter.json
-  set -x
+    set +x
+    field="$1"
+    value="$2"
+    cookiecutter_json=$(cat cookiecutter.json | jq ._$field=\"$value\" | jq ".__prompts__.$field=\"$field (latest=$value)\"" )
+    echo "$cookiecutter_json" > cookiecutter.json
+    set -x
 }
 
-uv pip compile --allow-unsafe --quiet cookiecutter-packages.in --output-file=cookiecutter-packages.txt
-update_field django_version "$(grep -oP '(?<=django==).*' cookiecutter-packages.txt | cut -d. -f1-2)"
-update_field pip_tools_version "$(grep -oP '(?<=pip-tools==).*' cookiecutter-packages.txt)"
-update_field pip_version "$(grep -oP '(?<=pip==).*' cookiecutter-packages.txt)"
+python_version="$(grep -oP '(?<="_python_version": ")(.*)(?=")' cookiecutter.json)"
+
+update_field python_version $python_version
 update_field dockerfile_version "$(grep -oP '(?<="_dockerfile_version": ")(.*)(?=")' cookiecutter.json)"
 update_field memcached_version "$(grep -oP '(?<="_memcached_version": ")(.*)(?=")' cookiecutter.json)"
 update_field postgresql_version "$(grep -oP '(?<="_postgresql_version": ")(.*)(?=")' cookiecutter.json)"
-update_field python_version "$(grep -oP '(?<="_python_version": ")(.*)(?=")' cookiecutter.json)"
 update_field redis_version "$(grep -oP '(?<="_redis_version": ")(.*)(?=")' cookiecutter.json)"
-update_field setuptools_version "$(grep -oP '(?<=setuptools==).*' cookiecutter-packages.txt)"
 update_field debian_release "$(grep -oP '(?<="_debian_release": ")(.*)(?=")' cookiecutter.json)"
-update_field uv_version "$(grep -oP '(?<=uv==).*' cookiecutter-packages.txt)"
 
+
+update_package() {
+    field="$1"
+    package="$2"
+
+    uv pip compile --python=python$python_version --upgrade --resolution=highest <(echo $package) --output-file=cookiecutter-packages.txt
+    update_field $field "$(grep -oPm1 '(?<='$package'==).*' cookiecutter-packages.txt)"
+}
+
+update_package django_version django
+update_package pip_tools_version pip-tools
+update_package pip_version pip
+update_package setuptools_version setuptools
+update_package uv_version uv
 
 rm cookiecutter-packages.txt
 
